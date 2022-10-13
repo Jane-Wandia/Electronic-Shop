ec3d2aa7ff0fbf2e25f3e42e9a7edef50a99a2fc
janekigenyi <janekigenyi@gmail.com>
2022-10-14 00:47:56 +0300

    imported switch from react router dom

--- src/components/front/Routes/Routes.js
+++ src/components/front/Routes/Routes.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Products from "../Products/Product";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch } from "react-router-dom";
 
 const Routes = () => {
-  return <div>
-   Routes
-  </div>;
-  
+  return <div>products</div>
+    
 };
-
 export default Routes;
