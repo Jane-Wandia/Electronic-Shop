c62387e13b8539d249a2ccfea02b581d73709ac3
janekigenyi <janekigenyi@gmail.com>
2022-10-13 21:17:35 +0300

    eddited the app

--- src/components/back/Data/Data.js
+++ src/components/back/Data/Data.js
@@ -1,58 +1,58 @@
-constdata = {
-    productItems:[
+const data = {
+    productItems: [
         {
             id: "1",
             name: "sol Headphone",
             price: "1050",
-            image:"./pics/photos/images/sol headphones.jpg",
+            image:"./pics/photos/images/sol headphones.jpg"
         },
         {
             id: "2",
             name: "Beats Headphone",
             price: "800",
-            image:"./pics/photos/images/Beats-Headphones.gif",
+            image:"./pics/photos/images/Beats-Headphones.gif"
         },
         {
             id: "3",
             name: "Iphone x",
             price: "5000",
-            image:"./pics/photos/images/iphone x.jpg",
+            image:"./pics/photos/images/iphone x.jpg"
         },
         {
             id: "4",
             name: "Oneplus",
             price: "2500",
-            image:"./pics/photos/images/Oneplus.jpg",
+            image:"./pics/photos/images/Oneplus.jpg"
         },
         {
             id: "5",
             name: "Macbook",
             price: "7999",
-            image:"./pics/photos/images/Macbook-m1.jpg",
+            image:"./pics/photos/images/Macbook-m1.jpg"
         },
         {
             id: "6",
             name: "samsung",
             price: "6999",
-            image:"./pics/photos/images/samsung.jpg",
+            image:"./pics/photos/images/samsung.jpg"
         },
         {
             id: "7",
             name: "Acer Laptop",
             price: "1099",
-            image:"./pics/photos/images/Acer Laptop.jpg",
+            image:"./pics/photos/images/Acer Laptop.jpg"
         },
         {
             id: "8",
             name: "Asus Laptop",
             price: "2999",
-            image:"./pics/photos/images/Asus Laptop.jpg",
+            image:"./pics/photos/images/Asus Laptop.jpg"
         },
         {
             id: "9",
             name: "Sennheiser",
             price: "3000",
-            image:"./pics/photos/images/sennheiser.jpg",
+            image:"./pics/photos/images/sennheiser.jpg"
         },
     ]
 }
